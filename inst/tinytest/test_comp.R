0ee99a4253f32c94a9d16315b16bd23653073709
rCarto <timothee.giraud@cnrs.fr>
2023-03-01 16:14:46 +0100

    test: add test for composed legends

---
+++ inst/tinytest/test_comp.R
@@ -0,0 +1,8 @@
+tinytest::expect_silent({
+  plot.new()
+  plot.window(xlim = c(0, 1), ylim = c(0, 1), asp = 1)
+  box()
+  leg_comp(type = "prop", val = c(10,50,100)) |>
+    leg_comp(type = "typo", val = c("A", "B", "C")) |>
+    leg_draw()
+})
